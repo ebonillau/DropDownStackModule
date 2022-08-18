2248c42610e97deda7b8dfc62b37f22a9bead58f
Ebonill <Ebonill@gap.com>
2022-08-18 16:46:57 -0500

    Add preview data

--- Sources/DropDownStackModule/DropDownStack.swift
+++ Sources/DropDownStackModule/DropDownStack.swift
@@ -36,42 +36,43 @@ public struct DropDownStack: View {
     }
 }
 
-//struct DropDownStack_Previews: PreviewProvider {
-//
-//    static private func getMedicalAppointmentsDropDownItem() -> DropDownItem {
-//        DropDownItem(title: "Title 1",
-//                     subtitle: "Subtitle 1",
-//                     scheduleItems: getMedicalAppointments())
-//    }
-//
-//    static private func getMedicalAppointments() -> [ScheduleItem] {
-//        var scheduleTypes: [ScheduleItem] = []
-//        scheduleTypes.append(contentsOf: [
-//            ScheduleItem(name: "Agendar 1", iconName: "note.text"),
-//            ScheduleItem(name: "Agendar 2", iconName: "person")
-//        ])
-//        return scheduleTypes
-//    }
-//
-//    static private func getClinicServicesDropDownItem() -> DropDownItem {
-//        DropDownItem(title: "Title 2",
-//                     subtitle: "Subtitle 2",
-//                     scheduleItems: getClinicServices())
-//    }
-//
-//    static private func getClinicServices() -> [ScheduleItem] {
-//        var scheduleTypes: [ScheduleItem] = []
-//        scheduleTypes.append(contentsOf: [
-//            ScheduleItem(name: "Agendar 3", iconName: "person"),
-//            ScheduleItem(name: "Agendar 4", iconName: "note.text"),
-//            ScheduleItem(name: "Agendar 4", iconName: "person")
-//        ])
-//        return scheduleTypes
-//    }
-//
-//    static var previews: some View {
-//        DropDownStack(dropDownItems: .constant([getMedicalAppointmentsDropDownItem(),
-//                                     getClinicServicesDropDownItem()]),
-//                      selectedScheduleItem: .constant(nil))
-//    }
-//}
+struct DropDownStack_Previews: PreviewProvider {
+
+    static private func getMedicalAppointmentsDropDownItem() -> DropDownItem {
+        DropDownItem(title: "Title 1",
+                     subtitle: "Subtitle 1",
+                     scheduleItems: getMedicalAppointments())
+    }
+
+    static private func getMedicalAppointments() -> [ScheduleItem] {
+        var scheduleTypes: [ScheduleItem] = []
+        scheduleTypes.append(contentsOf: [
+            ScheduleItem(name: "Agendar 1", iconName: "note.text"),
+            ScheduleItem(name: "Agendar 2", iconName: "person")
+        ])
+        return scheduleTypes
+    }
+
+    static private func getClinicServicesDropDownItem() -> DropDownItem {
+        DropDownItem(title: "Title 2",
+                     subtitle: "Subtitle 2",
+                     scheduleItems: getClinicServices())
+    }
+
+    static private func getClinicServices() -> [ScheduleItem] {
+        var scheduleTypes: [ScheduleItem] = []
+        scheduleTypes.append(contentsOf: [
+            ScheduleItem(name: "Agendar 3", iconName: "person"),
+            ScheduleItem(name: "Agendar 4", iconName: "note.text"),
+            ScheduleItem(name: "Agendar 4", iconName: "person")
+        ])
+        return scheduleTypes
+    }
+
+    static var previews: some View {
+        DropDownStack(dropDownItems: .constant([getMedicalAppointmentsDropDownItem(),
+                                     getClinicServicesDropDownItem()]),
+                      selectedScheduleItem: .constant(nil),
+                      selectedDropDownItem: nil)
+    }
+}
